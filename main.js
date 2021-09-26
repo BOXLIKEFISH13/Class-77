a33adfa9972f748fe80a89c6ce7702f17912ac1c
BOXLIKEFISH13 <85645331+BOXLIKEFISH13@users.noreply.github.com>
2021-09-26 11:11:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var a = 7 ;
+var b = 10 ;
+var add = a + b ;
+console.log(add);
+var subtract = a - b ;
+console.log(subtract);
+var multiplication = a * b ;
+console.log(multiplication);
+var divide = a / b ;
+console.log(divide)
